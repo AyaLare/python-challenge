325833f09463a9243d10a34263258802976b0f9a
Foluke <folusings@yahoo.com>
2023-08-10 22:49:47 +0800

    Module 3 challenge final submission

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,93 +11,101 @@ import csv
 # 5. The greatest decrease in profits (date and amount) over the entire period
 
 def main():
-   #
-   # Defining the key variables required for the analysis
-   #
-   total_number_of_months = 0
-   net_profit_losses = 0
-   rowcounter = 0
-   Previous_month_profit_loss = 0
-   total_profit_losses_change = 0
-   changes_in_profit_losses_month = 0
-   average_profit_loss = 0
-   greatest_increase_in_profits = 0
-   greatest_increase_date = ""
-   greatest_decrease_in_profits = 0
-   greatest_decrease_date = ""
-   date_list = []
-   profit_losses_list = []
-   # 
-   # Defining the path to the source data file to be analyzed
-   #       
-   csvpath = os.path.join("..","PyBank\Resources", "budget_data.csv")
-   #
-   # Define the file where the result of the analysis will be stored
-   #
-   output_path = os.path.join("..","PyBank\Analysis", "Pybank Assignment Result.txt")
+      #
+      # Defining the key variables required for the analysis
+      #
+      total_number_of_months = 0
+      net_profit_losses = 0
+      budgetcounter = 0
+      previous_month_profit_loss = 0
+      total_profit_losses_change = 0
+      changes_in_profit_losses_month = 0
+      changes_in_profit_list = []
+      average_profit_loss = 0
+      greatest_increase_in_profits = 0
+      greatest_increase_date = ""
+      greatest_decrease_in_profits = 0
+      greatest_decrease_date = ""
+      date_list = []
+      profit_losses_list = []
+      # 
+      # Defining the path to the source data file to be analyzed
+      #       
+      csvpath = os.path.join("..","PyBank\Resources", "budget_data.csv")
+      #
+      # Define the file where the result of the analysis will be stored
+      #
+      output_path = os.path.join("..","PyBank\Analysis", "Pybank Assignment Result.txt")
+      output_path2 = os.path.join("..","PyBank\Analysis", "Monthly Changes.txt")
 
-   with open(csvpath) as csvfile:
-         # CSV reader specifies delimiter and variable that holds contents
-         csvreader = csv.reader(csvfile, delimiter=',')
-         # print(csvreader)
-         #
-         # Read the header row first (skip this step if there is no header)
-         csv_header = next(csvreader)
-         # print(csv_header)
-         #        
-         # Read each row of data after the header
-         #
-         for row in csvreader:
-             rowcounter = rowcounter + 1
-             #print(row)
-             date_list.append(row[0])
-             #Picking the first column of each row holding the date information
-             datesection = row[0]
-             #print(datesection[0:3])
-             #Calculating the change in profit/loss for the current row with the previous one
-             changes_in_profit_losses_month = int(row[1]) - Previous_month_profit_loss
-             # Test to see if the change is bigger than the previous biggest change recorded
-             if changes_in_profit_losses_month > greatest_increase_in_profits:
-                   greatest_increase_in_profits = changes_in_profit_losses_month
-                   greatest_increase_date = datesection
+      with open(csvpath) as csvfile:
+            # CSV reader specifies delimiter and variable that holds contents
+            csvreader = csv.reader(csvfile, delimiter=',')
+            # print(csvreader)
+            #
+            # Read the header row first (skip this step if there is no header)
+            csv_header = next(csvreader)
+            # print(csv_header)
+            #        
+            # Read each row of data after the header
+            #
+            for row in csvreader:
+                  budgetcounter = budgetcounter + 1
+                  #print(row)
+                  date_list.append(row[0])
+                  #Picking the first column of each row holding the date information
+                  datesection = row[0]
+                  #print(datesection[0:3])
+                  #Calculating the change in profit/loss for the current row with the previous one
+                  if budgetcounter > 1:
+                        changes_in_profit_losses_month = int(row[1]) - previous_month_profit_loss
+                        #print(changes_in_profit_losses_month)
+                        changes_in_profit_list.append(int(row[1]) - previous_month_profit_loss)
+                        #print(changes_in_profit_list)
+                        # Test to see if the change is bigger than the previous biggest change recorded
+                        if changes_in_profit_losses_month > greatest_increase_in_profits:
+                              greatest_increase_in_profits = changes_in_profit_losses_month
+                              greatest_increase_date = datesection
+                        #Test to see if the change is smaller than the previous greatest loss change recorded
+                        if changes_in_profit_losses_month < greatest_decrease_in_profits:
+                              greatest_decrease_in_profits = changes_in_profit_losses_month
+                              greatest_decrease_date = datesection
+                        #
+                  # Calculate the total changes in profit and losses
+                  total_profit_losses_change = total_profit_losses_change + changes_in_profit_losses_month
+                               
+                  profit_losses_list.append(row[1])
+                  net_profit_losses = net_profit_losses + int(row[1])
+                  #Previous_date_month = row[0]
+                  previous_month_profit_loss = int(row[1])
+                  #total_profit_losses_change = total_profit_losses_change + changes_in_profit_losses_month
+                  # Generate and print the summary information from the data
+                  #        
+      total_number_of_months = len(date_list)
+      average_profit_loss = round(total_profit_losses_change / total_number_of_months,2)
+      print("Finanical Analysis")
+      print("_____________________________________")
+      print("Total Months: " + str(total_number_of_months))
+      print("Total:  $" +str(net_profit_losses))
+      print("Average Change: $", str(average_profit_loss))
+      print("Greatest Increase in Profits: ", greatest_increase_date, " ($",str(greatest_increase_in_profits),")" )
+      print("Greatest Decrease in Profits: ", greatest_decrease_date, " ($",str(greatest_decrease_in_profits),")" )
+      #
+      # open the file and write ther results
+      #
+      with open(output_path, 'w') as file:
+            file.writelines("Finanical Analysis" + "\n") 
+            file.writelines("_____________________________________" + "\n") 
+            file.writelines("Total Months: " + str(total_number_of_months) + "\n")
+            file.writelines("Total:  $" + str(net_profit_losses) + "\n")
+            file.writelines("Average Change: $" + str(average_profit_loss) + "\n")
+            file.writelines("Greatest Increase in Profits: " + greatest_increase_date + " ($" + str(greatest_increase_in_profits) + ")" + "\n" )
+            file.writelines("Greatest Decrease in Profits: " + greatest_decrease_date + " ($" + str(greatest_decrease_in_profits) + ")" + "\n" )
+      # Write the changes to a file to cross-check the results
+      #with open(output_path2,'w') as file:
+            #for row in changes_in_profit_list:
+                  #file.writelines(str(row) + "\n")
 
-            #Test to see if the change is smaller than the previous greatest loss change recorded
-             if changes_in_profit_losses_month < greatest_decrease_in_profits:
-                   greatest_decrease_in_profits = changes_in_profit_losses_month
-                   greatest_decrease_date = datesection
-             #
-             # Calculate the total changes in profit and losses
-             total_profit_losses_change = total_profit_losses_change + changes_in_profit_losses_month
-             #
-             # Check if the data is for the start of the year
-             #if datesection[0:3] == "Jan":
-             #  year_start_profit_loss = int(row[1])
-                        
-             profit_losses_list.append(row[1])
-             net_profit_losses = net_profit_losses + int(row[1])
-             #Previous_date_month = row[0]
-             Previous_month_profit_loss = int(row[1])
-   # Generate and print the summary information from the data        
-   total_number_of_months = len(date_list)
-   average_profit_loss = round(total_profit_losses_change / total_number_of_months,2)
-   print("Finanical Analysis")
-   print("_____________________________________")
-   print("Total Months: " + str(total_number_of_months))
-   print("Total:  $" +str(net_profit_losses))
-   print("Average Change: $", str(average_profit_loss))
-   print("Greatest Increase in Profits: ", greatest_increase_date, " ($",str(greatest_increase_in_profits),")" )
-   print("Greatest Decrease in Profits: ", greatest_decrease_date, " ($",str(greatest_decrease_in_profits),")" )
-   #
-   # open the file and write ther results
-   #
-   with open(output_path, 'w') as file:
-      file.writelines("Finanical Analysis" + "\n") 
-      file.writelines("_____________________________________" + "\n") 
-      file.writelines("Total Months: " + str(total_number_of_months) + "\n")
-      file.writelines("Total:  $" + str(net_profit_losses) + "\n")
-      file.writelines("Average Change: $" + str(average_profit_loss) + "\n")
-      file.writelines("Greatest Increase in Profits: " + greatest_increase_date + " ($" + str(greatest_increase_in_profits) + ")" + "\n" )
-      file.writelines("Greatest Decrease in Profits: " + greatest_decrease_date + " ($" + str(greatest_decrease_in_profits) + ")" + "\n" )
 main()
 
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,17 +15,15 @@ def main():
    #
     row_count = 0
     current_candidate_voted = " "
-    candidate_ballot = 0
+    candidate_ballot = 1
     candidate_count = 0
     candidate = "  "
     previous_candidate_total = 0
-    #vote_count = 0
     total_number_of_votes_cast = 0
-    ballot_id = 0
-    ballot_county = " "
-    ballot_voted_for = " "
+    popular_vote = 0
+    election_dictionary_keys = []
     #List_of_candidates_received_votes = []
-    #Percentage_votes_per_candidate = [ ]
+    #Percentage_votes_per_candidate = 0
     #Total_number_votes_per_candidate = 0
     #Winner_of_election_based_on_popular_vote = ""
     #
@@ -41,7 +39,7 @@ def main():
     #
     output_path = os.path.join("..","PyPoll\Analysis", "PyPoll Election Result.txt")
     with open(csvpath) as csvfile:
-#          # CSV reader specifies delimiter and variable that holds contents
+    # CSV reader specifies delimiter and variable that holds contents
         csvreader = csv.reader(csvfile, delimiter=',')
          #
          # Read the header row first (skip this step if there is no header)
@@ -50,49 +48,80 @@ def main():
          #        
          # Read each row of data after the header
          #
-        election_dictionary = {" ", 0}
+        #election_dictionary = {" ", 0}
+        election_dictionary = {}
         for row in csvreader:
             row_count = row_count + 1
-            ballot_id = int(row[0])
-            ballot_county = row[1]
-            ballot_voted_for = row[2]
+            #ballot_id = int(row[0])
+            #ballot_county = row[1]
+            #
+            #Test if the data retrieved is for a different candidate than previously held
+            #
+            #Candidate is the same, so keep adding the ballots to the ballot count for the candidate
             if row[2] == current_candidate_voted:
                 candidate_ballot = candidate_ballot + 1
-            
+            #Candidate is now different, so its time write the interim result for the previous candidate in memory
             if row[2] != current_candidate_voted:
+               #There is a new candidate, so increase the count
                candidate_count = candidate_count + 1
-               if row[2] not in election_dictionary:
-                    print(str(row[2]))
-                    candidate = row[2]
-                    election_dictionary.update({candidate: candidate_ballot})
-                    print(election_dictionary)
-               else:
-                    #previous_candidate_total = election_dictionary[candidate_count]  
+               #Check if the previous candidate already has votes stored in the election registry and check its not blank (first time)
+               if current_candidate_voted not in election_dictionary and current_candidate_voted != " ":
+                    #print(current_candidate_voted + "not yet in dictionary")
+                    #Write the interim ballot votes for the previous candidate into the registry
+                    election_dictionary.update({current_candidate_voted: candidate_ballot})
+                    #Check if the candidate's ballot count is the highest so far, if so make it the highest
+                    if candidate_ballot > popular_vote:
+                        popular_candidate = current_candidate_voted
+                        popular_vote = candidate_ballot
+               #The candidate already has a previous ballot count in the election registry
+               #The ballot count int he registry is to be updated with the interim count recently got
+               elif current_candidate_voted != " " :
+                    candidate_count = candidate_count - 1
+                    previous_candidate_total = election_dictionary.get(current_candidate_voted)  
                     previous_candidate_total = previous_candidate_total + candidate_ballot
-                    print(previous_candidate_total)
-                    election_dictionary.update({candidate_count: previous_candidate_total})
-                    print(election_dictionary)
-                     #election_dictionary.add(row[2]: previous_candidate_total)
-                    candidate_ballot = 1
-                    current_candidate_voted = row[2]
-            #print(str(ballot_id) + " " + ballot_county + " " + ballot_voted_for)
-            # Update the dictionary
-            # election_dictionary[str(row[2])] = row[2]
-            #election_dictionary
-    print(election_dictionary) 
-   # Generate and print the summary information from the data
+                    #print(current_candidate_voted + " Previous Votes: " + str(election_dictionary.get(current_candidate_voted)) + "  Additional Votes: " + str(candidate_ballot) + "  New Total Votes: " + str(previous_candidate_total))
+                    election_dictionary.update({current_candidate_voted: previous_candidate_total})
+                    #Check if the cumulaative ballot count for the candidate is highest and if so, change the highest score to reflect this info
+                    if previous_candidate_total > popular_vote:
+                           popular_candidate = current_candidate_voted
+                           popular_vote = previous_candidate_total
+               #Switch the candidate to the latest data read from the database
+               candidate_ballot = 1
+               current_candidate_voted = row[2]
+        #Update the registry for the last batch of data accumulated and conditionally update the popular vote i.e. the winner info
+        previous_candidate_total = election_dictionary.get(current_candidate_voted)  
+        previous_candidate_total = previous_candidate_total + candidate_ballot
+        election_dictionary.update({current_candidate_voted: previous_candidate_total})
+        if previous_candidate_total > popular_vote:
+             popular_candidate = current_candidate_voted
+        #print(election_dictionary)
+   # Generate and print the analysis summary information to the screen
    # 
-    total_number_of_votes_cast = row_count        
-    print("Election Results")
-    print("_____________________________________")
-    print("Total Votes: " + str(total_number_of_votes_cast))
-    print("_____________________________________")
+    total_number_of_votes_cast = row_count 
+    print("Election Results" + "\n")
+    print("_____________________________________" + "\n")
+    print("Total Votes: " + str(total_number_of_votes_cast) + "\n")
+    print("_____________________________________" + "\n")
+    election_dictionary_keys = election_dictionary.keys()
+    candidate_count = len(election_dictionary_keys)
+    for rows in election_dictionary_keys:
+        print(rows + ": "+ str(round(((election_dictionary.get(rows)/total_number_of_votes_cast)*100),3))+ "% " + "(" + str(election_dictionary.get(rows)) + ")" + "\n")
+    print("_____________________________________" + "\n")
+    print("Winner: " + popular_candidate )
+    print("_____________________________________" + "\n")
    #
-   # open the file and write ther results
+   # open the file to hold the analysis results in write mode
+   # Write the analsysis results to the 
    #
     with open(output_path, 'w') as file:
         file.writelines("Election Results" + "\n") 
         file.writelines("_____________________________________" + "\n") 
-        file.writelines("Total Months: " + str(total_number_of_votes_cast) + "\n")
+        file.writelines("\n" + "Total Votes: " + str(total_number_of_votes_cast) + "\n")
+        file.writelines("_____________________________________" + "\n") 
+        for rows in election_dictionary_keys:
+            file.writelines(rows + ": "+ str(round(((election_dictionary.get(rows)/total_number_of_votes_cast)*100),3))+ "% " + "(" + str(election_dictionary.get(rows)) + ")" + "\n")
         file.writelines("_____________________________________" + "\n")
+        file.writelines("\n" "Winner: " + popular_candidate + "\n")
+        file.writelines("_____________________________________" + "\n")
+
 main()
